02feed3e663fccbaf232bee1b9bca7f8d37cd233
Tyrel Parker <tyrel.parker@gmail.com>
2026-05-19 17:07:24 -0700

    fix: load directly to game

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -72,12 +72,12 @@ export class Game {
       if (this.state === 'MENU') {
         this.startGame()
       } else if (this.state === 'HIGH_SCORE_VIEW') {
-        this.state = 'MENU'
-        this.input.placeholder = 'type a word, press enter...'
+        this.startGame()
       }
     })
 
     input.focus()
+    this.startGame()
     this.loop(0)
   }
 
@@ -272,7 +272,7 @@ export class Game {
     }
 
     if (this.state === 'HIGH_SCORE_VIEW') {
-      if (e.key === 'Enter') { e.preventDefault(); this.state = 'MENU'; this.input.placeholder = 'press enter to start' }
+      if (e.key === 'Enter') { e.preventDefault(); this.startGame() }
       return
     }
 
